288cf483225d65fd9d5aee9df2238f83fe856ef4
sadhanareddy <sadhana.virupaksha@gmail.com>
2017-03-06 13:12:19 +0530

    add css

--- sample.js
+++ sample.js
@@ -2,7 +2,7 @@ function swich_sheets(){
 	    // appending a CSS stylesheet to head of webpage
 		var link = document.createElement('link');
 		// using rawgit.com MaxCDN.. files directly linked to git repo 
-        link.href = "https://cdn.rawgit.com/sadhanareddy/css-changer-tool/sample.js"; //random version number removed bcoz some browser take it as text file and not as CSS.
+        link.href = "https://cdn.rawgit.com/sadhanareddy/css-changer-tool/annolet.css"; //random version number removed bcoz some browser take it as text file and not as CSS.
     	link.type = "text/css";
     	link.rel = "stylesheet";
         document.getElementsByTagName('head')[0].appendChild(link);
