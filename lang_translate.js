14bc6d58e6f7800031a239bf7602fae5fc8a83c9
sadhanareddy <sadhana.virupaksha@gmail.com>
2017-03-13 19:46:09 +0530

    modified js

---
+++ lang_translate.js
@@ -0,0 +1,19 @@
+function googleTranslateElementInit() {
+new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+}
+
+(function swich_button(){
+     div = document.createElement('div');
+     div.id = 'google_translate_element';
+     body = document.getElementsByTagName('body')[0];
+     body.appendChild(div);
+}());
+
+// // funtion to disable all links
+// (function disableAllLinks(){
+//     var anchors = document.getElementsByTagName("a");
+//     for (var i = 0; i < anchors.length; i++) {
+//         anchors[i].onclick = function() {return(false);};
+//     }
+// }());
+
