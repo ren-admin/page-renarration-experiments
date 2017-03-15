c8b793965470d372979e9003e93471dc42993cc2
sadhanareddy <sadhana.virupaksha@gmail.com>
2017-03-15 11:11:01 +0530

    add js

--- lang_translate.js
+++ lang_translate.js
@@ -99,9 +99,10 @@ document.querySelector('#translate').addEventListener('click', function() {
                 //document.querySelector('#output').innerHTML = json.text[0];
                 //text= json.text[0];
                  if (document.selection && document.selection.createRange) {
-                      range = document.selection.createRange();
-                      //range.text = json.text[0];
-                      range.pasteHTML(json.text[0]);
+                      // range = document.selection.createRange();
+                      // //range.text = json.text[0];
+                      // range.pasteHTML(json.text[0]);
+                      alert(document.selection);
                   }
             }
             else {
