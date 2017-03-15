ef52e920a3f07d102cd2a21fb321935aedacbbdf
sadhanareddy <sadhana.virupaksha@gmail.com>
2017-03-15 16:32:54 +0530

    add js

---
+++ disablecss.js
@@ -0,0 +1,4 @@
+for ( i=0; i<document.styleSheets.length; i++) {
+    document.styleSheets.item(i).disabled=true;
+}
+
---
+++ phonetics.js
@@ -0,0 +1,6 @@
+(function swich_button(){
+     div = document.createElement('div');
+     div.id = 'google_translate_element';
+     body = document.getElementsByTagName('body')[0];
+     body.appendChild(div);
+}());
