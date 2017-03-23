21f34d7f57c74d5a12d160456d81bcbd03bb6778
sadhanareddy <sadhana.virupaksha@gmail.com>
2017-03-23 18:06:35 +0530

    add css

--- switch_css.js
+++ switch_css.js
@@ -1,8 +1,23 @@
 (function changeCss(){
-	var link = document.createElement('link');
-    link.rel  = 'stylesheet';
-    link.type = 'text/css';
-    link.href = 'switch1.css';
-    var head  = document.getElementsByTagName('head')[0];
-    head.appendChild(link);
+	var link_tags = 0;
+	if(link_tags == 0){
+		var link = document.createElement('link');
+	    link.rel  = 'stylesheet';
+	    link.type = 'text/css';
+	    link.href = 'switch1.css';
+	    link.title="switch1"
+	    var head  = document.getElementsByTagName('head')[0];
+	    head.appendChild(link);
+	    link_tags++;
+	}
+	else if(link_tags==1){
+		var link = document.createElement('link');
+	    link.rel  = 'alternative stylesheet';
+	    link.type = 'text/css';
+	    link.href = 'switch2.css';
+	    link.title="switch2"
+	    var head  = document.getElementsByTagName('head')[0];
+	    head.appendChild(link);
+	    link_tags++;
+	}
 }());
