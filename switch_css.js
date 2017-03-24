f445f0de68f02918e09d2a5346e73a57ace707d1
sadhanareddy <sadhana.virupaksha@gmail.com>
2017-03-24 15:08:07 +0530

    modified css

--- switch_css.js
+++ switch_css.js
@@ -17,25 +17,25 @@
 // *** END OF CUSTOMISABLE SECTION ***
 // You do not need to customise anything below this line
 
-switch_style('switch1')
+// switch_style('switch1')
 
-function switch_style (css_title)
-{
-  	alert("hello");
-  	var i,linktag;
-  	linktag = document.getElementsByTagName("link");
+// function switch_style (css_title)
+// {
+//   	alert("hello");
+//   	var i,linktag;
+//   	linktag = document.getElementsByTagName("link");
 
-  	for (i = 0; i < linktag.length; i++ ) {
-		if ((linktag[i].rel.indexOf( "stylesheet" ) != -1) &&linktag[i].title) {
-		  	linktag[i].disabled = true ;
-		  	if (linktag[i].title == css_title) {
-		    	linktag[i].disabled = false ;
-		    	alert(linktag[i])
-		  	}
-		}
-	// set_cookie( style_cookie_name, css_title,style_cookie_duration, style_domain );
-  	}
-}
+//   	for (i = 0; i < linktag.length; i++ ) {
+// 		if ((linktag[i].rel.indexOf( "stylesheet" ) != -1) &&linktag[i].title) {
+// 		  	linktag[i].disabled = true ;
+// 		  	if (linktag[i].title == css_title) {
+// 		    	linktag[i].disabled = false ;
+// 		    	alert(linktag[i])
+// 		  	}
+// 		}
+// 	// set_cookie( style_cookie_name, css_title,style_cookie_duration, style_domain );
+//   	}
+// }
 
 
 // function set_style_from_cookie()
