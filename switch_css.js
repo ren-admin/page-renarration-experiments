a18272af676cad83a6d0f0e7ff2ae04eb6d9712b
sadhanareddy <sadhana.virupaksha@gmail.com>
2017-03-24 12:04:10 +0530

    modified css

--- switch_css.js
+++ switch_css.js
@@ -21,20 +21,21 @@ switch_style('switch1')
 
 function switch_style ( css_title )
 {
-  var i, link_tag ;
-  link_tag = document.getElementsByTagName("link") ;
-  for (i = 0, i < link_tag.length ; i++ ) {
+	alert("hello");
+  // var i, link_tag ;
+  // link_tag = document.getElementsByTagName("link") ;
+  // for (i = 0, i < link_tag.length ; i++ ) {
 
-		if ((link_tag[i].rel.indexOf( "stylesheet" ) != -1) &&link_tag[i].title) {
-		  	link_tag[i].disabled = true ;
-		  	if (link_tag[i].title == css_title) {
-		    	link_tag[i].disabled = false ;
-		    	alert(link_tag[i])
-		  	}
-		}
+		// if ((link_tag[i].rel.indexOf( "stylesheet" ) != -1) &&link_tag[i].title) {
+		//   	link_tag[i].disabled = true ;
+		//   	if (link_tag[i].title == css_title) {
+		//     	link_tag[i].disabled = false ;
+		//     	alert(link_tag[i])
+		//   	}
+		// }
 
-    //set_cookie( style_cookie_name, css_title,style_cookie_duration, style_domain );
-  }
+  //   //set_cookie( style_cookie_name, css_title,style_cookie_duration, style_domain );
+  // }
 }
 
 
