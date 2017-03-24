ff79524d02334bb2c9dccf5cd15e211a1cb0dc55
sadhanareddy <sadhana.virupaksha@gmail.com>
2017-03-24 11:13:28 +0530

    modified css

--- switch_css.js
+++ switch_css.js
@@ -1,13 +1,9 @@
-(function changeCss(){
-	var link_tags = 0;
-	if(link_tags == 0){
+(function (){
 		var link = document.createElement('link');
 	    link.rel  = 'stylesheet';
 	    link.type = 'text/css';
 	    link.href = 'switch1.css';
-	    link.title="switch1"
+	    link.title= 'switch1'
 	    var head  = document.getElementsByTagName('head')[0];
 	    head.appendChild(link);
-	    link_tags++;
-	}
 }());
