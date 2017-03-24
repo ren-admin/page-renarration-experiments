9cb8946272d08b6c84046db945ed95afe5496f13
sadhanareddy <sadhana.virupaksha@gmail.com>
2017-03-24 17:00:05 +0530

    modified css

--- switch_css.js
+++ switch_css.js
@@ -140,7 +140,7 @@ function switch_style (css_title)
 		    	linktag[i].disabled = false ;
 		  	}
 		}
-	set_cookie( style_cookie_name, css_title, style_cookie_duration, style_domain );
+	//set_cookie( style_cookie_name, css_title, style_cookie_duration, style_domain );
   	}
 }
 
