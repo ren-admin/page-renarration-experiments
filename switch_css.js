12b8d66692a2f892de96d150c052a1aec2d658f4
sadhanareddy <sadhana.virupaksha@gmail.com>
2017-03-24 11:39:10 +0530

    modified css

--- switch_css.js
+++ switch_css.js
@@ -3,7 +3,7 @@
 	    link.rel  = 'stylesheet';
 	    link.type = 'text/css';
 	    link.href = 'switch1.css';
-	    link.title= 'switch1'
+	    // link.title= 'switch1'
 	    head  = document.getElementsByTagName('head')[0];
 	    head.appendChild(link);
 }());
