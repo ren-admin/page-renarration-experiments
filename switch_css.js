62b2f553e6164e12916d6e5548f3271613924fae
sadhanareddy <sadhana.virupaksha@gmail.com>
2017-03-25 18:35:49 +0530

    add css

--- switch_css.js
+++ switch_css.js
@@ -131,21 +131,21 @@
 // var style_domain = "www.vlabs.ac.in" ;
 
 
-function switch_style (css_title)
-{	
-  	var i;
-  	var linktag = document.getElementsByTagName("link");
+// function switch_style (css_title)
+// {	
+//   	var i;
+//   	var linktag = document.getElementsByTagName("link");
 
-  	for (i = 0; i < linktag.length; i++ ) {
-		if ((linktag[i].rel.indexOf( "stylesheet" ) != -1) &&linktag[i].title) {
-		  	linktag[i].disabled = true ;
-		  	if (linktag[i].title == css_title) {
-		    	linktag[i].disabled = false ;
-		  	}
-		}
-	//set_cookie( style_cookie_name, css_title, style_cookie_duration, style_domain );
-  	}
-}
+//   	for (i = 0; i < linktag.length; i++ ) {
+// 		if ((linktag[i].rel.indexOf( "stylesheet" ) != -1) &&linktag[i].title) {
+// 		  	linktag[i].disabled = true ;
+// 		  	if (linktag[i].title == css_title) {
+// 		    	linktag[i].disabled = false ;
+// 		  	}
+// 		}
+// 	//set_cookie( style_cookie_name, css_title, style_cookie_duration, style_domain );
+//   	}
+// }
 
 // function set_cookie ( cookie_name, cookie_value, lifespan_in_days, valid_domain )
 // {
