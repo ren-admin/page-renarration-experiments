e717907bd5c5f9c1bb383845987d04edde0da07b
sadhanareddy <sadhana.virupaksha@gmail.com>
2017-03-25 18:53:08 +0530

    add css

--- switch_css.js
+++ switch_css.js
@@ -118,8 +118,9 @@
 		text = document.createTextNode(text_node[i]);
 		button_tag.id = id[i];
 		button_tag.appendChild(text);
-		button_tag.addEventListener("click", switch_style(link_title[i]), false);
+		// button_tag.addEventListener("click", switch_style(link_title[i]), false);
 		//button_tag.addEventListener("click", hello, false);
+		button_tag.onclick = switch_style(link_title[i]);
 		body = document.getElementsByTagName('body')[0];
 		body.appendChild(button_tag);
 	}
