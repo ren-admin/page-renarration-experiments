5f848f0237eebc3a8ddf9f2f367572f346d89cef
sadhanareddy <sadhana.virupaksha@gmail.com>
2017-03-25 17:24:48 +0530

    add css

--- switch_css.js
+++ switch_css.js
@@ -91,7 +91,8 @@
 	var i= 0;
 	var css_links = 2;
 	var link_rel =['stylesheet', 'alternate stylesheet']
-	var css_themes =['switch1.css', 'switch2.css'];
+	var css_themes =['//github.com/sadhanareddy/css-changer-tool/blob/master/switch1.css', 
+	'//github.com/sadhanareddy/css-changer-tool/blob/master/switch2.css'];
 	link_title =['switch1', 'switch2'];
 	for(i=0; i<css_links; i++){
 		
@@ -142,45 +143,45 @@ function switch_style (css_title)
 		    	linktag[i].disabled = false ;
 		  	}
 		}
-	//set_cookie( style_cookie_name, css_title, style_cookie_duration, style_domain );
+	set_cookie( style_cookie_name, css_title, style_cookie_duration, style_domain );
   	}
 }
 
-// function set_cookie ( cookie_name, cookie_value, lifespan_in_days, valid_domain )
-// {
-//     var domain_string = valid_domain ?
-//                        ("; domain=" + valid_domain) : '' ;
-//     document.cookie = cookie_name +
-//                        "=" + encodeURIComponent( cookie_value ) +
-//                        "; max-age=" + 60 * 60 *
-//                        24 * lifespan_in_days +
-//                        "; path=/" + domain_string ;
-// }
+function set_cookie ( cookie_name, cookie_value, lifespan_in_days, valid_domain )
+{
+    var domain_string = valid_domain ?
+                       ("; domain=" + valid_domain) : '' ;
+    document.cookie = cookie_name +
+                       "=" + encodeURIComponent( cookie_value ) +
+                       "; max-age=" + 60 * 60 *
+                       24 * lifespan_in_days +
+                       "; path=/" + domain_string ;
+}
 
-// function get_cookie ( cookie_name )
-// {
-//     var name = cookie_name + "=";
-//     var decodedCookie = decodeURIComponent(document.cookie);
-//     var ca = decodedCookie.split(';');
-//     for(var i = 0; i <ca.length; i++) {
-//         var c = ca[i];
-//         while (c.charAt(0) == ' ') {
-//             c = c.substring(1);
-//         }
-//         if (c.indexOf(name) == 0) {
-//             return c.substring(name.length, c.length);
-//         }
-//     }
-//     return "";
-// }
+function get_cookie ( cookie_name )
+{
+    var name = cookie_name + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
 
-// function set_style_from_cookie()
-// {
-//   var css_title = get_cookie( style_cookie_name );
-//   if (css_title.length) {
-//     switch_style( css_title );
-//   }
-// }
+function set_style_from_cookie()
+{
+  var css_title = get_cookie( style_cookie_name );
+  if (css_title.length) {
+    switch_style( css_title );
+  }
+}
 
 
 window.onload = function () {
