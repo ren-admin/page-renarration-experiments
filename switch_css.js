ea666bc2b32d9910400fc72a08e756860793202a
sadhanareddy <sadhana.virupaksha@gmail.com>
2017-03-25 17:51:14 +0530

    add css

--- switch_css.js
+++ switch_css.js
@@ -91,7 +91,8 @@
 	var i= 0;
 	var css_links = 2;
 	var link_rel =['stylesheet', 'alternate stylesheet']
-	var css_themes =['switch1.css', 'switch2.css'];
+	var css_themes =['//github.com/sadhanareddy/css-changer-tool/blob/master/switch1.css',
+	'//github.com/sadhanareddy/css-changer-tool/blob/master/switch2.css'];
 	link_title =['switch1', 'switch2'];
 	for(i=0; i<css_links; i++){
 		
@@ -117,7 +118,7 @@
 		text = document.createTextNode(text_node[i]);
 		button_tag.id = id[i];
 		button_tag.appendChild(text);
-		button_tag.addEventListener("click", switch_style(link_title[i]), false);
+		//button_tag.addEventListener("click", switch_style(link_title[i]), false);
 		//button_tag.addEventListener("click", hello, false);
 		body = document.getElementsByTagName('body')[0];
 		body.appendChild(button_tag);
@@ -125,9 +126,9 @@
 }());
 
 
-var style_cookie_name = "style" ;
-var style_cookie_duration = 30 ;
-var style_domain = "www.vlabs.ac.in" ;
+// var style_cookie_name = "style" ;
+// var style_cookie_duration = 30 ;
+// var style_domain = "www.vlabs.ac.in" ;
 
 
 function switch_style (css_title)
@@ -137,53 +138,47 @@ function switch_style (css_title)
 
   	for (i = 0; i < linktag.length; i++ ) {
 		if ((linktag[i].rel.indexOf( "stylesheet" ) != -1) &&linktag[i].title) {
-		   	linktag[i].disabled = true ;
+		  	linktag[i].disabled = true ;
 		  	if (linktag[i].title == css_title) {
 		    	linktag[i].disabled = false ;
 		  	}
 		}
-	set_cookie( style_cookie_name, css_title, style_cookie_duration, style_domain );
+	//set_cookie( style_cookie_name, css_title, style_cookie_duration, style_domain );
   	}
 }
 
-function set_cookie ( cookie_name, cookie_value, lifespan_in_days, valid_domain )
-{
-    var domain_string = valid_domain ?
-                       ("; domain=" + valid_domain) : '' ;
-    document.cookie = cookie_name +
-                       "=" + encodeURIComponent( cookie_value ) +
-                       "; max-age=" + 60 * 60 *
-                       24 * lifespan_in_days +
-                       "; path=/" + domain_string ;
-}
+// function set_cookie ( cookie_name, cookie_value, lifespan_in_days, valid_domain )
+// {
+//     var domain_string = valid_domain ?
+//                        ("; domain=" + valid_domain) : '' ;
+//     document.cookie = cookie_name +
+//                        "=" + encodeURIComponent( cookie_value ) +
+//                        "; max-age=" + 60 * 60 *
+//                        24 * lifespan_in_days +
+//                        "; path=/" + domain_string ;
+// }
 
-function get_cookie ( cookie_name )
-{
-    var name = cookie_name + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
+// function get_cookie ( cookie_name )
+// {
+//     var name = cookie_name + "=";
+//     var decodedCookie = decodeURIComponent(document.cookie);
+//     var ca = decodedCookie.split(';');
+//     for(var i = 0; i <ca.length; i++) {
+//         var c = ca[i];
+//         while (c.charAt(0) == ' ') {
+//             c = c.substring(1);
+//         }
+//         if (c.indexOf(name) == 0) {
+//             return c.substring(name.length, c.length);
+//         }
+//     }
+//     return "";
+// }
 
-function set_style_from_cookie()
-{
-  var css_title = get_cookie( style_cookie_name );
-  if (css_title.length) {
-    switch_style( css_title );
-  }
-}
-
-
-window.onload = function () {
-      alert("LOADED!");
-      //set_style_from_cookie()
-}
+// function set_style_from_cookie()
+// {
+//   var css_title = get_cookie( style_cookie_name );
+//   if (css_title.length) {
+//     switch_style( css_title );
+//   }
+// }
