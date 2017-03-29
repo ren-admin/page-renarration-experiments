d2ca6b85e2112b2b35ac6d786e6552ea7c77bf70
sadhanareddy <sadhana.virupaksha@gmail.com>
2017-03-29 23:08:36 +0530

    modified js

--- switch_css.js
+++ switch_css.js
@@ -1,13 +1,13 @@
-// This functions creates two links tags, used to switch the css.
-(function insertStylesheets(){
+// This functions creates two link tags, for switching the style sheets.
+(function alternate_stylesheets(){
 	var i= 0;
 	var css_links = 2;
-	var link_rel =['stylesheet', 'alternate stylesheet']
-	var css_themes =['//rawgit.com/sadhanareddy/css-changer-tool/master/switch1.css',
-	'//rawgit.com/sadhanareddy/css-changer-tool/master/switch2.css'];
+	var link_rel =['stylesheet', 'alternate stylesheet'] 
+	var css_themes =['https://rawgit.com/sadhanareddy/css-changer-tool/master/switch1.css',
+	'https://github.com/sadhanareddy/css-changer-tool/blob/master/switch2.css'];
 	var link_title =['switch1', 'switch2'];
+
 	for(i=0; i<css_links; i++){
-		
 		link = document.createElement('link');
 	    link.rel  = link_rel[i];
 	    link.type = 'text/css';
@@ -16,27 +16,39 @@
 	    head  = document.getElementsByTagName('head')[0];
 	    head.appendChild(link);
 	}
-
 }());
 
-// Creates buttons to switch the css on click event. 
-(function createButton(){
-	var i= 0;
-	var buttons = 5;
-	var text_node = ['Theme_1', 'Theme_2', 'Theme_3', 'Increase_font', 'Decrease_font'];
-	var id = ['theme_1', 'theme_2','theme_3', 'increase_font', 'decrease_font'];
-	div = document.createElement("div");
-	div.id= 'annolet';
-	document.getElementsByTagName('body')[0].appendChild(div);
+/* This function creates a continer div which lets the users interact with app. When switchcssContainer
+is called, it injects CSS stylesheet into <head> element of website to stylize the container and
+<div id=css-container>...</div> whose inner HTML is a list of buttons inside body. */
 
+(function switchcss_container(){
+	// appending a CSS stylesheet to head of webpage
+    // var link = document.createElement('link');
+    // // using rawgit.com MaxCDN.. files directly linked to git repo 'annoletjs/master'
+    // link.rel = "stylesheet";
+    // link.type = "text/css";
+    // link.href = "https://rawgit.com/sadhanareddy/css-changer-tool/master/annolet.css"; //random version number removed bcoz some browser take it as text file and not as CSS.
+   	// document.getElementsByTagName('head')[0].appendChild(link);
+
+	// appending a div to body of webpage
+    var body = document.getElementsByTagName('body')[0];
+    var switchcss_container = document.createElement('div');
+    switchcss_container.id = 'switchcss-container';
+    body.appendChild(switchcss_container);
+
+    //appending buttons into the div inner HTML
+	var i= 0;
+	var buttons = 3;
+	var text_node = ['Theme_1', 'Theme_2', 'Theme_3'];
+	var button_id = ['theme_1', 'theme_2','theme_3'];
 	for(i=0; i<buttons; i++){
 		button_tag = document.createElement("BUTTON");
 		text = document.createTextNode(text_node[i]);
-		button_tag.id = id[i];
+		button_tag.id = button_id[i];
 		button_tag.appendChild(text);
-		document.getElementById('annolet').appendChild(button_tag);
+		document.getElementById('switchcss_container').appendChild(button_tag);
 	}
-
 }());
 
 document.getElementById('theme_1').addEventListener('click', function() {
@@ -46,114 +58,11 @@ document.getElementById('theme_2').addEventListener('click', function() {
 	switch_style('switch2')
 }, false);
 
-document.getElementById('theme_3').addEventListener('click', function() {
-	setbackground()
-}, false);
-
-
-(function changeFontsize(){
-	var fontSize = parseInt($('body').css('font-size'),10);
-    $('#increase_font').on('click',function(){
-        fontSize+=0.5;
-        $('body').css('font-size',fontSize+'px');
-    })
-    $('#decrease_font').on('click',function(){
-        fontSize-=0.5;
-        $('body').css('font-size',fontSize+'px');
-    })
-}());
+// document.getElementById('theme_3').addEventListener('click', function() {
+// 	setbackground()
+// }, false);
 
 
 
-function setbackground()
-{
-		window.setTimeout( "setbackground()", 3000); // 5000 milliseconds delay
-
-		var index = Math.round(Math.random() * 9);
-
-		var ColorValue = "FFFFFF"; // default color - white (index = 0)
-
-		if(index == 1)
-		ColorValue = "FFCCCC"; //peach
-		if(index == 2)
-		ColorValue = "CCAFFF"; //violet
-		if(index == 3)
-		ColorValue = "A6BEFF"; //lt blue
-		if(index == 4)
-		ColorValue = "99FFFF"; //cyan
-		if(index == 5)
-		ColorValue = "D5CCBB"; //tan
-		if(index == 6)
-		ColorValue = "99FF99"; //lt green
-		if(index == 7)
-		ColorValue = "FFFF99"; //lt yellow
-		if(index == 8)
-		ColorValue = "FFCC99"; //lt orange
-		if(index == 9)
-		ColorValue = "CCCCCC"; //lt grey
-
-		document.getElementsByTagName("body")[0].style.backgroundColor = "#" + ColorValue;
-}
-
-
-var style_cookie_name = "style";
-var style_cookie_duration = 30;
-var style_domain = "www.vlabs.ac.in";
-
-function switch_style (css_title)
-{	
-	alert(css_title);
-  	var i;
-  	var linktag = document.getElementsByTagName("link");
-
-  	for (i = 0; i < linktag.length; i++ ) {
-		if ((linktag[i].rel.indexOf( "stylesheet" ) != -1) &&linktag[i].title) {
-		  	linktag[i].disabled = true ;
-		  	if (linktag[i].title == css_title) {
-		    	linktag[i].disabled = false ;
-		  	}
-		}
-	set_cookie( style_cookie_name, css_title, style_cookie_duration, style_domain );
-  	}
-}
-
-function set_cookie ( cookie_name, cookie_value, lifespan_in_days, valid_domain )
-{
-    var domain_string = valid_domain ?
-                       ("; domain=" + valid_domain) : '' ;
-    document.cookie = cookie_name +
-                       "=" + encodeURIComponent( cookie_value ) +
-                       "; max-age=" + 60 * 60 *
-                       24 * lifespan_in_days +
-                       "; path=/" + domain_string ;
-    alert(document.cookie);
-}
-
-function get_cookie ( cookie_name )
-{
-    var cookie_string = document.cookie ;
-    if (cookie_string.length != 0) {
-        var cookie_value = cookie_string.match (
-                        '(^|;)[\s]*' +
-                        cookie_name +
-                        '=([^;]*)' );
-        return decodeURIComponent ( cookie_value[2] ) ;
-    }
-    return '' ;
-}
-
-function set_style_from_cookie()
-{
-  var css_title = get_cookie( style_cookie_name );
-  if (css_title.length) {
-    switch_style( css_title );
-  }
-}
-
-window.onload=function () {
-	// insertStylesheets()
-	// createButton()
-	set_style_from_cookie()
-}
 
 
